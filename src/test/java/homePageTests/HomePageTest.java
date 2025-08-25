305c04ed393fa5264867c75cc2861d6c993c7d41
mary <repetskamary@gmail.com>
2025-08-25 17:00:49 +0300

    завдання 4

--- src/test/java/homePageTests/HomePageTest.java
+++ src/test/java/homePageTests/HomePageTest.java
@@ -106,6 +106,54 @@ public class HomePageTest {
 
         driver.quit();
     }
+    // 4 завдання
+    @Test
+    public void checkHeaderButtons() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://allo.ua/");
+
+        WebElement buttonPokupciamDisplayed = driver.findElement(By.xpath("//div[@class='mh-button__wrap']"));
+
+        Assert.assertTrue(buttonPokupciamDisplayed.isDisplayed());
+
+        sleep(5000);
+
+        buttonPokupciamDisplayed.click();
+
+        sleep(5000);
+
+        WebElement dropDownMenuOpened = driver.findElement(By.xpath("//div[@class='mh-button__dropdown']"));
+
+        Assert.assertTrue(dropDownMenuOpened.isDisplayed());
+
+        WebElement buttonDostavkaiOplataDisplayed = driver.findElement(By.xpath("(//a[@class='mh-button'])[5]"));
+
+        Assert.assertTrue(buttonDostavkaiOplataDisplayed.isDisplayed());
+
+        buttonDostavkaiOplataDisplayed.click();
+
+        WebElement checkTitleOplataiDostavka = driver.findElement(By.xpath("//h2[@class='sp-page-title sp-h2 page-header']"));
+
+        String textOplataiDostavkaCheck = checkTitleOplataiDostavka.getText();
+
+        //System.out.println(textOplataiDostavkaCheck);
+
+        Assert.assertTrue(textOplataiDostavkaCheck.contains("Доставка і оплата"));
+
+        WebElement yakOformytyZamovleniaDisplayed = driver.findElement(By.xpath("(//h3[@class='sub-block-header'])[1]"));
+
+        Assert.assertTrue(yakOformytyZamovleniaDisplayed.isDisplayed());
+
+        String checkyakOformytyZamovlenia = yakOformytyZamovleniaDisplayed.getText();
+
+        Assert.assertTrue(checkyakOformytyZamovlenia.contains("Як оформити замовлення?"));
+
+        driver.quit();
+
+    }
 
 }
 
